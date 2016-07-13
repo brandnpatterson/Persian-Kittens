905f22e7b93631e96584bf7bb0ba21da82b074bf
Brandon Patterson <brandonnpatterson@gmail.com>
2016-07-13 18:22:14 -0500

    Added back to top button and made nav background visible for mobile

---
+++ app/scripts/back-up.js
@@ -0,0 +1,15 @@
+
+
+$(document).ready(function() {
+
+  $("#backUp").hide();
+
+  $(window).scroll(function() {
+      if ($(window).scrollTop() > 800) {
+          $("#backUp").fadeIn("slow");
+      }
+      else {
+          $("#backUp").fadeOut("fast");
+      }
+  });
+});
