f2a465f06e4bbc9f7e7e8c71377c802a0805de5b
Brandon Patterson <brandonnpatterson@gmail.com>
2016-07-13 16:33:50 -0500

    styled the page. need to fix nav

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,35 +1,203 @@
-$(document).ready(function() {
-
-  // Hide the entire wrapper when the document loads.
-  $("#males, #females, #grayKittens, #whiteKittens, #shows").hide();
-
-  // Show only males.
-  $("#malesNav").click(function() {
-    $("#logo, #females, #grayKittens, #whiteKittens, #shows").hide();
-    $("#males").show();
-  });
-
-  // Show only females.
-  $("#femalesNav").click(function() {
-    $("#logo, #males, #grayKittens, #whiteKittens, #shows").hide();
-    $("#females").show();
-  });
-
-  // Show only grayKittens.
-  $("#grayKittensNav").click(function() {
-    $("#logo, #males, #females, #whiteKittens, #shows").hide();
-    $("#grayKittens").show();
-  });
-
-  // Show only whiteKittens.
-  $("#whiteKittensNav").click(function() {
-    $("#logo, #males, #females, #grayKittens, #shows").hide();
-    $("#whiteKittens").show();
-  });
-
-  // Show only shows.
-  $("#showsNav").click(function() {
-    $("#logo, #males, #females, #grayKittens, #whiteKittens").hide();
-    $("#shows").show();
-  });
+$(document).scroll(function() {
+  var y = $(this).scrollTop();
+  if (y > 800) {
+    $('.backUp').fadeIn();
+  } else {
+    $('.backUp').fadeOut();
+  }
 });
+
+$(document).scroll(function() {
+  var y = $(this).scrollTop();
+  if (y > 800) {
+    $('.backUp').fadeIn();
+  } else {
+    $('.backUp').fadeOut();
+  }
+});
+
+
+
+// $(document).ready(function() {
+//
+//   // Hide the entire wrapper when the document loads.
+//   $("#males, #females, #grayKittens, #whiteKittens, #shows").hide();
+//
+//   // Show only males.
+//   $("#malesNav").click(function() {
+//     $("#logo, #females, #grayKittens, #whiteKittens, #shows").hide();
+//     $("#males").show();
+//   });
+//
+//   // Show only females.
+//   $("#femalesNav").click(function() {
+//     $("#logo, #males, #grayKittens, #whiteKittens, #shows").hide();
+//     $("#females").show();
+//   });
+//
+//   // Show only grayKittens.
+//   $("#grayKittensNav").click(function() {
+//     $("#logo, #males, #females, #whiteKittens, #shows").hide();
+//     $("#grayKittens").show();
+//   });
+//
+//   // Show only whiteKittens.
+//   $("#whiteKittensNav").click(function() {
+//     $("#logo, #males, #females, #grayKittens, #shows").hide();
+//     $("#whiteKittens").show();
+//   });
+//
+//   // Show only shows.
+//   $("#showsNav").click(function() {
+//     $("#logo, #males, #females, #grayKittens, #whiteKittens").hide();
+//     $("#shows").show();
+//   });
+// });
+
+
+// $(document).ready(function() {
+//
+//   // Hide the entire wrapper when the document loads.
+//   $("#males, #females, #grayKittens, #whiteKittens, #shows").hide();
+//
+//   // Show only males.
+//   $("#malesNav").click(function() {
+//     $("#logo, #females, #grayKittens, #whiteKittens, #shows").hide();
+//     $("#males").show();
+//   });
+//
+//   // Show only females.
+//   $("#femalesNav").click(function() {
+//     $("#logo, #males, #grayKittens, #whiteKittens, #shows").hide();
+//     $("#females").show();
+//   });
+//
+//   // Show only grayKittens.
+//   $("#grayKittensNav").click(function() {
+//     $("#logo, #males, #females, #whiteKittens, #shows").hide();
+//     $("#grayKittens").show();
+//   });
+//
+//   // Show only whiteKittens.
+//   $("#whiteKittensNav").click(function() {
+//     $("#logo, #males, #females, #grayKittens, #shows").hide();
+//     $("#whiteKittens").show();
+//   });
+//
+//   // Show only shows.
+//   $("#showsNav").click(function() {
+//     $("#logo, #males, #females, #grayKittens, #whiteKittens").hide();
+//     $("#shows").show();
+//   });
+// });
+
+
+// $(document).ready(function() {
+//
+//   // Hide the entire wrapper when the document loads.
+//   $("#males, #females, #grayKittens, #whiteKittens, #shows").hide();
+//
+//   // Show only males.
+//   $("#malesNav").click(function() {
+//     $("#logo, #females, #grayKittens, #whiteKittens, #shows").hide();
+//     $("#males").show();
+//   });
+//
+//   // Show only females.
+//   $("#femalesNav").click(function() {
+//     $("#logo, #males, #grayKittens, #whiteKittens, #shows").hide();
+//     $("#females").show();
+//   });
+//
+//   // Show only grayKittens.
+//   $("#grayKittensNav").click(function() {
+//     $("#logo, #males, #females, #whiteKittens, #shows").hide();
+//     $("#grayKittens").show();
+//   });
+//
+//   // Show only whiteKittens.
+//   $("#whiteKittensNav").click(function() {
+//     $("#logo, #males, #females, #grayKittens, #shows").hide();
+//     $("#whiteKittens").show();
+//   });
+//
+//   // Show only shows.
+//   $("#showsNav").click(function() {
+//     $("#logo, #males, #females, #grayKittens, #whiteKittens").hide();
+//     $("#shows").show();
+//   });
+// });
+
+
+// $(document).ready(function() {
+//
+//   // Hide the entire wrapper when the document loads.
+//   $("#males, #females, #grayKittens, #whiteKittens, #shows").hide();
+//
+//   // Show only males.
+//   $("#malesNav").click(function() {
+//     $("#logo, #females, #grayKittens, #whiteKittens, #shows").hide();
+//     $("#males").show();
+//   });
+//
+//   // Show only females.
+//   $("#femalesNav").click(function() {
+//     $("#logo, #males, #grayKittens, #whiteKittens, #shows").hide();
+//     $("#females").show();
+//   });
+//
+//   // Show only grayKittens.
+//   $("#grayKittensNav").click(function() {
+//     $("#logo, #males, #females, #whiteKittens, #shows").hide();
+//     $("#grayKittens").show();
+//   });
+//
+//   // Show only whiteKittens.
+//   $("#whiteKittensNav").click(function() {
+//     $("#logo, #males, #females, #grayKittens, #shows").hide();
+//     $("#whiteKittens").show();
+//   });
+//
+//   // Show only shows.
+//   $("#showsNav").click(function() {
+//     $("#logo, #males, #females, #grayKittens, #whiteKittens").hide();
+//     $("#shows").show();
+//   });
+// });
+
+$(document).scroll(function(){var c=$(this).scrollTop();c>800?$(".backUp").fadeIn():$(".backUp").fadeOut()});
+// $(document).ready(function() {
+//
+//   // Hide the entire wrapper when the document loads.
+//   $("#males, #females, #grayKittens, #whiteKittens, #shows").hide();
+//
+//   // Show only males.
+//   $("#malesNav").click(function() {
+//     $("#logo, #females, #grayKittens, #whiteKittens, #shows").hide();
+//     $("#males").show();
+//   });
+//
+//   // Show only females.
+//   $("#femalesNav").click(function() {
+//     $("#logo, #males, #grayKittens, #whiteKittens, #shows").hide();
+//     $("#females").show();
+//   });
+//
+//   // Show only grayKittens.
+//   $("#grayKittensNav").click(function() {
+//     $("#logo, #males, #females, #whiteKittens, #shows").hide();
+//     $("#grayKittens").show();
+//   });
+//
+//   // Show only whiteKittens.
+//   $("#whiteKittensNav").click(function() {
+//     $("#logo, #males, #females, #grayKittens, #shows").hide();
+//     $("#whiteKittens").show();
+//   });
+//
+//   // Show only shows.
+//   $("#showsNav").click(function() {
+//     $("#logo, #males, #females, #grayKittens, #whiteKittens").hide();
+//     $("#shows").show();
+//   });
+// });
