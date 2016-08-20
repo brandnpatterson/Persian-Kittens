ae8ee982ebf1e505a53c82b9168b7e5a3a4980ae
Brandon Patterson <brandonnpatterson@gmail.com>
2016-08-20 16:21:00 -0500

    refreshed jquery

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -26,49 +26,4 @@ $(document).ready(function() {
   });
 });
 
-$(document).ready(function() {
-
-  $("#backUp").hide();
-
-  $(window).scroll(function() {
-      if ($(window).scrollTop() > 800) {
-          $("#backUp").fadeIn("slow");
-      }
-      else {
-          $("#backUp").fadeOut("fast");
-      }
-  });
-});
-
-$(document).ready(function() {
-
-  $("#backUp").hide();
-
-  $(window).scroll(function() {
-      if ($(window).scrollTop() > 800) {
-          $("#backUp").fadeIn("slow");
-      }
-      else {
-          $("#backUp").fadeOut("fast");
-      }
-  });
-});
-
-$(document).ready(function() {
-
-  $("#backUp").hide();
-
-  $(window).scroll(function() {
-      if ($(window).scrollTop() > 800) {
-          $("#backUp").fadeIn("slow");
-      }
-      else {
-          $("#backUp").fadeOut("fast");
-      }
-  });
-});
-
 $(document).ready(function(){$("#backUp").hide(),$(window).scroll(function(){$(window).scrollTop()>800?$("#backUp").fadeIn("slow"):$("#backUp").fadeOut("fast")})});
-$(document).ready(function(){$("#backUp").hide(),$(window).scroll(function(){$(window).scrollTop()>800?$("#backUp").fadeIn("slow"):$("#backUp").fadeOut("fast")})}),$(document).ready(function(){$("#backUp").hide(),$(window).scroll(function(){$(window).scrollTop()>800?$("#backUp").fadeIn("slow"):$("#backUp").fadeOut("fast")})}),$(document).ready(function(){$("#backUp").hide(),$(window).scroll(function(){$(window).scrollTop()>800?$("#backUp").fadeIn("slow"):$("#backUp").fadeOut("fast")})});
-$(document).ready(function(){$("#backUp").hide(),$(window).scroll(function(){$(window).scrollTop()>800?$("#backUp").fadeIn("slow"):$("#backUp").fadeOut("fast")})}),$(document).ready(function(){$("#backUp").hide(),$(window).scroll(function(){$(window).scrollTop()>800?$("#backUp").fadeIn("slow"):$("#backUp").fadeOut("fast")})}),$(document).ready(function(){$("#backUp").hide(),$(window).scroll(function(){$(window).scrollTop()>800?$("#backUp").fadeIn("slow"):$("#backUp").fadeOut("fast")})}),$(document).ready(function(){$("#backUp").hide(),$(window).scroll(function(){$(window).scrollTop()>800?$("#backUp").fadeIn("slow"):$("#backUp").fadeOut("fast")})}),$(document).ready(function(){$("#backUp").hide(),$(window).scroll(function(){$(window).scrollTop()>800?$("#backUp").fadeIn("slow"):$("#backUp").fadeOut("fast")})}),$(document).ready(function(){$("#backUp").hide(),$(window).scroll(function(){$(window).scrollTop()>800?$("#backUp").fadeIn("slow"):$("#backUp").fadeOut("fast")})}),$(document).ready(function(){$("#backUp").hide(),$(window).scroll(function(){$(window).scrollTop()>800?$("#backUp").fadeIn("slow"):$("#backUp").fadeOut("fast")})});
-$(document).ready(function(){$("#backUp").hide(),$(window).scroll(function(){$(window).scrollTop()>800?$("#backUp").fadeIn("slow"):$("#backUp").fadeOut("fast")})}),$(document).ready(function(){$("#backUp").hide(),$(window).scroll(function(){$(window).scrollTop()>800?$("#backUp").fadeIn("slow"):$("#backUp").fadeOut("fast")})}),$(document).ready(function(){$("#backUp").hide(),$(window).scroll(function(){$(window).scrollTop()>800?$("#backUp").fadeIn("slow"):$("#backUp").fadeOut("fast")})}),$(document).ready(function(){$("#backUp").hide(),$(window).scroll(function(){$(window).scrollTop()>800?$("#backUp").fadeIn("slow"):$("#backUp").fadeOut("fast")})}),$(document).ready(function(){$("#backUp").hide(),$(window).scroll(function(){$(window).scrollTop()>800?$("#backUp").fadeIn("slow"):$("#backUp").fadeOut("fast")})}),$(document).ready(function(){$("#backUp").hide(),$(window).scroll(function(){$(window).scrollTop()>800?$("#backUp").fadeIn("slow"):$("#backUp").fadeOut("fast")})}),$(document).ready(function(){$("#backUp").hide(),$(window).scroll(function(){$(window).scrollTop()>800?$("#backUp").fadeIn("slow"):$("#backUp").fadeOut("fast")})}),$(document).ready(function(){$("#backUp").hide(),$(window).scroll(function(){$(window).scrollTop()>800?$("#backUp").fadeIn("slow"):$("#backUp").fadeOut("fast")})}),$(document).ready(function(){$("#backUp").hide(),$(window).scroll(function(){$(window).scrollTop()>800?$("#backUp").fadeIn("slow"):$("#backUp").fadeOut("fast")})}),$(document).ready(function(){$("#backUp").hide(),$(window).scroll(function(){$(window).scrollTop()>800?$("#backUp").fadeIn("slow"):$("#backUp").fadeOut("fast")})}),$(document).ready(function(){$("#backUp").hide(),$(window).scroll(function(){$(window).scrollTop()>800?$("#backUp").fadeIn("slow"):$("#backUp").fadeOut("fast")})}),$(document).ready(function(){$("#backUp").hide(),$(window).scroll(function(){$(window).scrollTop()>800?$("#backUp").fadeIn("slow"):$("#backUp").fadeOut("fast")})}),$(document).ready(function(){$("#backUp").hide(),$(window).scroll(function(){$(window).scrollTop()>800?$("#backUp").fadeIn("slow"):$("#backUp").fadeOut("fast")})}),$(document).ready(function(){$("#backUp").hide(),$(window).scroll(function(){$(window).scrollTop()>800?$("#backUp").fadeIn("slow"):$("#backUp").fadeOut("fast")})}),$(document).ready(function(){$("#backUp").hide(),$(window).scroll(function(){$(window).scrollTop()>800?$("#backUp").fadeIn("slow"):$("#backUp").fadeOut("fast")})});
