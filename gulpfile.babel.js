dbcb3e6046aef94175a7b97e6a60e383bc160439
Brandon Patterson <brandonnpatterson@gmail.com>
2016-09-25 19:01:54 -0500

    gh-pages initial commit

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -13,16 +13,12 @@ import      sync from "browser-sync"
 const $ = load()
 const reload = sync.reload
 
-gulp.task('build', ['html', 'fonts', 'images'])
+gulp.task('build', ['html', 'lint', 'fonts', 'images'])
 
 gulp.task('clean', del.bind(null, ['app/css/style.min.css', 'app/js/main.js', 'app/js/main.min.js', 'app/js/**.min.js', 'dist/css/style.min.css', 'dist/fonts', 'dist/images', 'dist/index.html', 'dist/js/main.min.js'], {read: false}))
 
 gulp.task('default', ['html', 'lint', 'fonts', 'images'], () => {
   gulp.start('serve')
-})
-
-gulp.task('dist', ['clean'], () => {
-  gulp.start('serve:dist', ['build'])
 })
 
 gulp.task('fonts', () => {
@@ -34,7 +30,7 @@ gulp.task('html', ['scripts', 'styles'], () => {
   return gulp.src('app/*.html')
     .pipe($.useref({searchPath: ['app']}))
     .pipe($.htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('./'))
 })
 
 gulp.task('images', () => {
@@ -58,7 +54,7 @@ gulp.task('serve', () => {
   sync({
     notify: false,
     server: {
-      baseDir: 'app'
+      baseDir: './'
     }
   })
 
@@ -68,21 +64,11 @@ gulp.task('serve', () => {
   gulp.watch('app/js/*.js', ['scripts'])
 })
 
-gulp.task('serve:dist', () => {
-  sync({
-    notify: false,
-    server: {
-      baseDir: 'dist'
-    }
-  })
-})
-
 gulp.task('scripts', () => {
   return gulp.src('app/js/*.js')
     .pipe(concat('main.js'))
     .pipe($.uglify())
     .pipe($.rename({suffix: '.min'}))
-    .pipe(gulp.dest('app/js'))
     .pipe(gulp.dest('dist/js'))
 })
 
@@ -91,7 +77,6 @@ gulp.task('styles', () => {
   .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
   .pipe(rename({suffix: '.min'}))
   .pipe(prefix('last 2 versions'))
-  .pipe(gulp.dest('app/css'))
   .pipe(gulp.dest('dist/css'))
   .pipe(reload({stream: true}))
 })
