f30a30fd6ed9a975bcd33c6d691f2f32556873f5
Brandon Patterson <brandonnpatterson@gmail.com>
2016-11-14 11:37:09 -0600

    gulpfile ;;;

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,24 +1,24 @@
-import gulp       from 'gulp'
-import concat     from 'gulp-concat'
-import del        from 'del'
-import eslint     from 'gulp-eslint'
-import load       from 'gulp-load-plugins'
-import prefix     from 'gulp-autoprefixer'
-import rename     from 'gulp-rename'
-import sass       from 'gulp-sass'
-import sourcemaps from 'gulp-sourcemaps'
-import sync       from 'browser-sync'
+import gulp       from 'gulp';
+import concat     from 'gulp-concat';
+import del        from 'del';
+import eslint     from 'gulp-eslint';
+import load       from 'gulp-load-plugins';
+import prefix     from 'gulp-autoprefixer';
+import rename     from 'gulp-rename';
+import sass       from 'gulp-sass';
+import sourcemaps from 'gulp-sourcemaps';
+import sync       from 'browser-sync';
 
-const $ = load()
-const reload = sync.reload
+const $ = load();
+const reload = sync.reload;
 
-gulp.task('build', ['html', 'lint'])
+gulp.task('build', ['html', 'lint']);
 
-gulp.task('clean', del.bind(null, ['index.html', 'style.css', 'app/assets/views/*', 'dist/views/*', 'dist/*.min.js'], {read: false}))
+gulp.task('clean', del.bind(null, ['index.html', 'style.css', 'app/assets/views/*', 'dist/views/*', 'dist/*.min.js'], {read: false}));
 
 gulp.task('default', ['build', 'watch'], () => {
-  gulp.start('serve')
-})
+  gulp.start('serve');
+});
 
 gulp.task('html', ['scripts', 'styles'], () => {
   return gulp.src('app/*.html')
@@ -26,8 +26,8 @@ gulp.task('html', ['scripts', 'styles'], () => {
     .pipe($.useref({searchPath: ['app']}))
     .pipe($.htmlmin({collapseWhitespace: true}))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./'))
-})
+    .pipe(gulp.dest('./'));
+});
 
 gulp.task('images', () => {
   return gulp.src('app/images/*')
@@ -36,17 +36,17 @@ gulp.task('images', () => {
       interlaced: true,
       svgoPlugins: [{cleanupIDs: false}]
     })))
-    .pipe(gulp.dest('dist/images'))
-})
+    .pipe(gulp.dest('dist/images'));
+});
 
 gulp.task('lint', () => {
   return gulp.src(['*/**/*.js', '!node_modules/*', '!test/*'])
     .pipe(eslint())
     .pipe(eslint.format())
-    .pipe(eslint.failAfterError())
-})
+    .pipe(eslint.failAfterError());
+});
 
-gulp.task('rebuild', ['clean', 'default'])
+gulp.task('rebuild', ['clean', 'default']);
 
 gulp.task('scripts', () => {
   return gulp.src('app/js/*.js')
@@ -56,8 +56,8 @@ gulp.task('scripts', () => {
     .pipe($.uglify())
     .pipe($.rename({suffix: '.min'}))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('dist'))
-})
+    .pipe(gulp.dest('dist'));
+});
 
 gulp.task('serve', () => {
   sync({
@@ -66,8 +66,8 @@ gulp.task('serve', () => {
       baseDir: './'
     },
     port: 8888
-  })
-})
+  });
+});
 
 gulp.task('styles', () => {
   gulp.src('app/css/style.scss')
@@ -75,11 +75,11 @@ gulp.task('styles', () => {
   .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
   .pipe(prefix('last 2 versions'))
   .pipe(sourcemaps.write())
-  .pipe(gulp.dest('./'))
-})
+  .pipe(gulp.dest('./'));
+});
 
 gulp.task('watch', () => {
     gulp.watch('app/*.html', ['html', reload])
     gulp.watch('app/css/**/*.*', ['styles', reload])
-    gulp.watch('app/js/*.js', ['scripts', reload])
-})
+    gulp.watch('app/js/*.js', ['scripts', reload]);
+});
